8ed4887832552f4c26a257cc06cfcffa7096235e
Yu Chen <ychen2@z3t5c1.42bangkok.com>
2024-01-12 11:29:55 +0700

    edit test

--- Module01/ex03/main.cpp
+++ Module01/ex03/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yu <yu@student.42.fr>                      +#+  +:+       +#+        */
+/*   By: ychen2 <ychen2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 23:25:37 by yu                #+#    #+#             */
-/*   Updated: 2024/01/05 23:25:39 by yu               ###   ########.fr       */
+/*   Updated: 2024/01/12 11:29:07 by ychen2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int main(void) {
 	{
 		Weapon club = Weapon("crude spiked club");
 		HumanB jim("Jim");
+		jim.attack();
 		jim.setWeapon(club);
 		jim.attack();
 		club.setType("some other type of club");
