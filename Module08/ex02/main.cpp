5680679175465676b019b28424755e7f6c8e5ec0
Yu Chen <ychen2@z3t4c1.42bangkok.com>
2024-04-26 19:28:59 +0700

    add some tests

--- Module08/ex02/main.cpp
+++ Module08/ex02/main.cpp
@@ -6,14 +6,16 @@
 /*   By: ychen2 <ychen2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 22:12:06 by ychen2            #+#    #+#             */
-/*   Updated: 2024/03/08 00:25:33 by ychen2           ###   ########.fr       */
+/*   Updated: 2024/04/26 19:25:49 by ychen2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 #include <iostream>
+#include <list>
 
 int main() {
+	std::cout << "MutantStack:\n";
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
@@ -36,5 +38,28 @@ int main() {
 	}
 	std::cout << std::endl;
 	std::stack<int> s(mstack);
+
+	std::cout << "List:\n";
+	std::list<int> lst;
+	lst.push_back(5);
+	lst.push_back(17);
+	std::cout << *lst.rbegin() << std::endl;
+	lst.pop_back();
+	std::cout << lst.size() << std::endl;
+	lst.push_back(3);
+	lst.push_back(5);
+	lst.push_back(737);
+	//[...]
+	lst.push_back(0);
+	MutantStack<int>::iterator it_lst = mstack.begin();
+	MutantStack<int>::iterator ite_lst = mstack.end();
+	++it_lst;
+	--it_lst;	
+	while (it_lst != ite_lst)
+	{
+		std::cout << *it_lst << " ";
+		++it_lst;
+	}
+	std::cout << std::endl;
 	return 0;
 }
